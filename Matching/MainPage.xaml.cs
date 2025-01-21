90a7d9277dc241dc466ec5c69bc878bf5d08cd62
mikebutts <mgbutts@gmail.com>
2025-01-21 17:14:49 -0500

    Working visible match game

--- Matching/MainPage.xaml.cs
+++ Matching/MainPage.xaml.cs
@@ -2,7 +2,11 @@
 {
     public partial class MainPage : ContentPage
     {
- 
+        Button lastClicked;
+        bool findingMatch = false;
+        int matchesFound;
+        int tenthsOfSecondsElapsed = 0;
+
         public MainPage()
         {
             InitializeComponent();
@@ -12,6 +16,8 @@
         {
             AnimalButtons.IsVisible = true;
             PlayAgainButton.IsVisible = false;
+            MatchesFoundLbl.IsVisible = true;
+
 
             List<string> animalList = [
                 "🦁", "🦁", 
@@ -31,11 +37,55 @@
                 button.Text = nextEmoji;
                 animalList.RemoveAt(index);
             };
+
+            Dispatcher.StartTimer(TimeSpan.FromSeconds(.1), TimerTick);
+        }
+
+        private bool TimerTick()
+        {
+            if (!this.IsLoaded) return false;
+
+            tenthsOfSecondsElapsed++;
+            TimeElapsed.Text = "Time elapsed: " + (tenthsOfSecondsElapsed / 10F).ToString("0.0s");
+
+            if (PlayAgainButton.IsVisible) {
+                   tenthsOfSecondsElapsed = 0;
+                return false;
+            }
+
+            return true;
+                
         }
 
         private void Button_Clicked(object sender, EventArgs e)
         {
+            if (sender is Button buttonClicked)
+            {
+                if (!string.IsNullOrWhiteSpace(buttonClicked.Text) && (findingMatch == false))
+                    {
+                    buttonClicked.BackgroundColor = Colors.Salmon;
+                    lastClicked = buttonClicked;
+                    findingMatch = true;
+                }
+                else
+                {
+                    if ((buttonClicked != lastClicked) && (buttonClicked.Text == lastClicked.Text)) {
+                        matchesFound++;
+                        MatchesFoundLbl.Text = "Matches Found: " + matchesFound.ToString();
+                        lastClicked.Text = " ";
+                        buttonClicked.Text = " ";
+                    }
+                    lastClicked.BackgroundColor = Colors.LightBlue;
+                    buttonClicked.BackgroundColor = Colors.LightBlue;
+                    findingMatch = false;
 
+                }
+            }
+            if (matchesFound == 8) {
+                matchesFound = 0;
+                AnimalButtons.IsVisible = false;
+                PlayAgainButton.IsVisible = true;
+            }
         }
     }
 
